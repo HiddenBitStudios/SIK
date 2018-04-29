ee0f5dfa810efd298ac911e0b52847bd8a237f55
Nicolas Papenfuß <nicolas.papenfuss@gmail.com>
2018-04-29 22:00:12 +0200

    restify

--- api.js
+++ api.js
@@ -1,12 +1,10 @@
 class API {
-    constructor(port, creds) {
-        if(typeof creds != "object" || !creds.user || !creds.pass) {
-            console.warn("The given Credentials are not complete")
-        } else {
-            this.pass = creds.pass
-            this.user = creds.user
-        }
-        this.port = port
+    constructor() {
+        const restify = require("restify")
+        const restifyPlugins = require('restify').plugins
+        this.server = restify.createServer({
+            name: 'SIK'
+        });
+        this.server.use(restify.plugins.bodyParser({}))
     }
-    
 }
