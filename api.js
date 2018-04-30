880b185b1a3240dbe7970a7593bef81553b21236
Nicolas Papenfuß <nicolas.papenfuss@gmail.com>
2018-04-30 16:53:40 +0200

    restify

--- api.js
+++ api.js
@@ -1,10 +1,15 @@
 class API {
     constructor() {
         const restify = require("restify")
+        const errors = requrie("restify-errors")
         const restifyPlugins = require('restify').plugins
+        this.port = 2499;
         this.server = restify.createServer({
             name: 'SIK'
         });
         this.server.use(restify.plugins.bodyParser({}))
+        this.server.listen(port, () => {
+            console.log("API running on", port)
+        });
     }
 }
