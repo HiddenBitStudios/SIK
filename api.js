6ec1b8c325b485683b067db2d29fa7fa7387c634
PixLSteam <PixLSteam@users.noreply.github.com>
2018-05-01 19:38:10 +0200

    Did stuff

--- api.js
+++ api.js
@@ -1,34 +1,46 @@
+const util = require("./util.js");
 class API {
-    constructor(port) {
-        this.port = port
-        this.isRunning = false
-        const restify = require("restify")
-        const errors = require("restify-errors")
-        const restifyPlugins = require('restify').plugins
-        this.server = restify.createServer({
-            name: 'SIK'
-        });
-        this.server.use(restify.plugins.bodyParser({}))
-        this.createRoutes()
-    }
-    isRunning() {
-        return this.isRunning
-    }
-    start() {
-        this.server.listen(this.port, () => {
-            this.isRunning = true
-        });
-    }
-    createRoutes() {
-        this.server.pre((req, res, next) => {
-            console.info(`${req.method} - ${req.url}`)
-            return next()
-        })
-        this.server.get("/api/info", (req, res, next) => {
-            res.send(200,"hey")
-            return next()
-        })
-    }
+	constructor(options) {
+		let opts = util.cloneRecursive(API.defaultOptions);
+		Object.assign(opts, options);
+		this.port = opts.port;
+		this._isRunning = false;
+		const restify = require("restify");
+		const errors = require("restify-errors");
+		const restifyPlugins = require('restify').plugins;
+		this.server = restify.createServer({
+			name: 'SIK'
+		});
+		this.server.use(restify.plugins.bodyParser({}));
+		this.createRoutes();
+	}
+	isRunning() {
+		return this._isRunning;
+	}
+	start() {
+		this.server.listen(this.port, () => {
+			this._isRunning = true;
+		});
+	}
+	createRoutes() {
+		this.server.pre((req, res, next) => {
+			console.info(`${req.method} - ${req.url}`);
+			return next();
+		});
+		this.server.get("/api/info", (req, res, next) => {
+			res.send(200,"hey");
+			return next();
+		});
+	}
 }
+API.defaultOptions = {
+	port: 2498,
+	sql: {
+		host: "localhost",
+		port: 3306,
+		user: "",
+		pass: ""
+	}
+};
 
 module.exports = API;
---
+++ util.js
@@ -0,0 +1,31 @@
+let util = {};
+util.cloneRecursive = function cloneRecursive(obj) {
+	// var recurTable = {};
+	var recurArrayOld = [];
+	var recurArrayNew = [];
+	var f;
+	f = function(ov) {
+		var nv = ov;
+		var i = recurArrayOld.indexOf(ov);
+		if (i >= 0) {
+			nv = recurArrayNew[i];
+		} else if (ov instanceof Array) {
+			nv = Array.prototype.slice.apply(ov);
+		} else if (typeof nv == "object") {
+			nv = {};
+			// recurTable[ov] = nv;
+			for (var k in ov) {
+				if (!ov.hasOwnProperty(k)) {
+					continue;
+				}
+				var v = ov[k];
+				nv[k] = f(v);
+			}
+		}
+		// recurTable[ov] = nv;
+		recurArrayOld.push(ov);
+		recurArrayNew.push(nv);
+		return nv;
+	}
+	return f(obj);
+};
